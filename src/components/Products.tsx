4f231942bdf53d1191c1f01d8ab6e1abfda3f729
Matt Seccafien <mseccafien@gmail.com>
2023-06-09 12:50:05 +0200

    Add previous step link and progress bar

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,29 +6,27 @@ import {
   generatePath,
   ActionFunctionArgs,
   redirect,
+  useLocation,
+  Link,
+  useLoaderData,
+  Form,
 } from 'react-router-dom'
-
-import { Step } from './Step'
-import { Summary } from './Summary'
 
 import * as api from '../lib/api'
 import { ProductType } from '../types'
 
 export function loader({ params }: LoaderFunctionArgs) {
-  console.log('Product loader')
-  // check if we have a query param in the url
-  // if so, we need to redirect to the correct step
-  return {}
+  return api.get()
 }
 
 export async function action({ request, params }: ActionFunctionArgs) {
-  const { product = '' } = params
+  const product = params.product as ProductType
   const form = await request.formData()
   const data = formDataToObject(form)
   const search = new URL(request.url).searchParams
   const step = search.get('step') || '0'
 
-  await api.save(data)
+  await api.save({ ...data, product })
   search.set('step', String(Number(step) + 1))
 
   const nextStepUrl = generatePath(`/buy/:product`, { product })
@@ -60,7 +58,6 @@ export function ProductDeveloperInsurance() {
   return (
     <section className="Product">
       <h1>Developer Insurance</h1>
-      <h2>Step </h2>
       <Steps>
         <EmailStep />
         <FullNameStep />
@@ -74,7 +71,6 @@ export function ProductDesignerInsurance() {
   return (
     <section className="Product">
       <h1>Designer Insurance</h1>
-      <h2>Step</h2>
       <Steps>
         <EmailStep />
         <FullNameStep />
@@ -85,34 +81,71 @@ export function ProductDesignerInsurance() {
 
 export function Steps({ children }: React.PropsWithChildren<{}>) {
   const [params] = useSearchParams()
+  const data = useLoaderData() as ReturnType<typeof loader>
+  const step = Number(params.get('step') || '0')
   const childrenArray = React.Children.toArray(children)
-  console.log(params.get('step'))
-  const currentStep = childrenArray[Number(params.get('step'))] || <Summary />
+  const location = useLocation()
+  const search = new URLSearchParams(location.search)
+  search.set('step', String(step - 1))
 
-  return <div className="Steps">{currentStep}</div>
+  const currentStep = childrenArray[step] || <Summary data={data} />
+
+  const previousLink =
+    step > 0 ? (
+      <Link to={`${location.pathname}?${search.toString()}`}>Back</Link>
+    ) : null
+
+  const progress = (
+    <progress
+      className="Progress"
+      max={childrenArray.length}
+      value={step}
+    ></progress>
+  )
+
+  return (
+    <div className="Steps">
+      {progress}
+      {previousLink}
+      {currentStep}
+    </div>
+  )
 }
 
 export function AgeStep() {
-  console.log(useParams())
+  const data = useLoaderData() as ReturnType<typeof loader>
+
   return (
     <Step>
-      <label>
-        Age <input id="age" name="age" type="number" />
+      <label htmlFor="age" className="Label">
+        <span>Age</span>
+        <input
+          id="age"
+          name="age"
+          type="number"
+          min="16"
+          max="120"
+          required
+          defaultValue={data.age}
+        />
       </label>
     </Step>
   )
 }
 
 export function EmailStep() {
+  const data = useLoaderData() as ReturnType<typeof loader>
+
   return (
     <Step>
-      <label>
-        Email{' '}
+      <label htmlFor="email" className="Label">
+        <span>Email</span>
         <input
-          id="age"
+          id="email"
           name="email"
           type="email"
-          defaultValue="mseccafien@gmail.com"
+          defaultValue={data.email}
+          required
         />
       </label>
     </Step>
@@ -120,20 +153,64 @@ export function EmailStep() {
 }
 
 export function FullNameStep() {
+  const data = useLoaderData() as ReturnType<typeof loader>
+
   return (
     <Step>
-      <label>
-        First Name <input id="firstName" name="firstName" type="text" />
+      <label htmlFor="firstName" className="Label">
+        <span>First name</span>
+        <input
+          id="firstName"
+          name="firstName"
+          required
+          defaultValue={data.firstName}
+        />
       </label>
-      <label>
-        Last Name <input id="lastName" name="lastName" type="text" />
+      <label htmlFor="lastName" className="Label">
+        <span>Last name</span>
+        <input
+          id="lastName"
+          name="lastName"
+          required
+          defaultValue={data.lastName}
+        />
       </label>
     </Step>
   )
 }
 
+export const Step: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <Form className="Step" method="post">
+      {children}
+      <button type="submit">Next</button>
+    </Form>
+  )
+}
+
+export const Summary: React.FC<{ data: Record<string, string> }> = ({
+  data,
+}: {
+  data: Record<string, string>
+}) => {
+  return (
+    <>
+      <h2>Summary</h2>
+      <dl>
+        {Object.entries(data).map(([key, value]) => (
+          <React.Fragment key={key}>
+            <dt>{key}</dt>
+            <dd>{value}</dd>
+          </React.Fragment>
+        ))}
+      </dl>
+    </>
+  )
+}
+
 function formDataToObject(formData: FormData): Record<string, string> {
   const object: Record<string, string> = {}
+
   formData.forEach((value, key) => {
     if (typeof value === 'string') {
       object[key] = value
